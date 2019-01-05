ce28a0ef39c8d0a31a419a2c6f049bed0660bc7c
hhalterman <hhalter@bgsu.edu>
2019-01-05 08:22:11 -0500

    code refactored a bit

--- assets/js/project_scripts.js
+++ assets/js/project_scripts.js
@@ -2,12 +2,12 @@ var previousBtn = document.querySelector(".project__main__btn--previous");
 var nextBtn = document.querySelector(".project__main__btn--next");
 var projectContainer = document.querySelector(".project__container");
 
-function showNextPage(){
+function showNextPage() {
   projectContainer.classList.toggle("project__container--next");
 }
 nextBtn.addEventListener("click", showNextPage);
 
-function showPreviousPage(){
+function showPreviousPage() {
   projectContainer.classList.toggle("project__container--previous");
 }
 previousBtn.addEventListener("click", showPreviousPage);
